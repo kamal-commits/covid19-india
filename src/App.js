eb8a8adbed4c4ec5b3fe871c08063b5fe249c136
Kamal Shah <54033333+kkamal714@users.noreply.github.com>
2020-05-02 20:50:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Card from './components/Card/Card';
+import StateTable from './components/StateTable/StateTable';
+import Axios from 'axios';
+import './components/StateTable/style.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({
+    allStates: [],
+    loading: false,
+  });
+
+  const { allStates, loading } = data;
+  const fetchAllData = async () => {
+    const url = 'https://api.covid19india.org/data.json';
+    const {
+      data: { statewise },
+    } = await Axios.get(url);
+    //Removing the First Element Of An Array
+    statewise.splice(0, 1);
+    const ExtractedData = {
+      statewise: statewise,
+    };
+
+    setData({ ...data, allStates: ExtractedData.statewise, loading: true });
+    // console.log(statewise);
+  };
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">COVID19-INDIA</h1>
+
+      <Card />
+
+      <div className="container mt-2">
+        <table className="table  table-bordered text-center table-hover">
+          <thead>
+            <tr>
+              <th className="text-secondary">Country</th>
+              <th className="text-danger">Confirmed</th>
+              <th className="text-primary">Active</th>
+              <th className="text-success">Recovered</th>
+              <th className="text-secondary">Deaths</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {allStates.map((stateName) => {
+              return <StateTable stateData={stateName} key={stateName.state} />;
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card/OriginalCard.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import CountUp from 'react-countup';
+import '../StateTable/style.css';
+
+const OriginalCard = ({
+  cases: {
+    confirmed,
+    deaths,
+    recovered,
+    deltaconfirmed,
+    deltadeaths,
+    deltarecovered,
+    lastupdatedtime,
+  },
+}) => {
+  //   console.log(confirmed);
+  if (!confirmed) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <div>
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-md-4 col-sm-12">
+            <div className="card text-center" style={{ width: '18rem' }}>
+              <div className="card-header text-primary font-weight-bold">
+                Total Confirmed
+              </div>
+              <div className="list-group list-group-flush">
+                <span className="deltainc mt-2 text-center">
+                  {confirmed > 0 ? <span> [+ {deltaconfirmed}] </span> : null}
+                </span>
+                <h3 className="font-weight-bold text-primary text-center ">
+                  <CountUp
+                    start={0}
+                    end={confirmed}
+                    separator=","
+                    duration={2.5}
+                  />
+                </h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4 col-sm-12">
+            <div className="card text-center" style={{ width: '18rem' }}>
+              <div className="card-header text-primary font-weight-bold">
+                Total Recovered
+              </div>
+              <div className="list-group list-group-flush">
+                <span className="deltainc2 mt-2 text-center">
+                  {recovered > 0 ? <span> [+ {deltarecovered}] </span> : null}
+                </span>
+                <h3 className="font-weight-bold text-success text-center ">
+                  <CountUp
+                    start={0}
+                    end={recovered}
+                    separator=","
+                    duration={2.5}
+                  />
+                </h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4 col-sm-12">
+            <div className="card text-center" style={{ width: '18rem' }}>
+              <div className="card-header text-primary font-weight-bold">
+                Total Deaths
+              </div>
+              <div className="list-group list-group-flush">
+                <span className="deltainc3 mt-2 text-center">
+                  {deaths > 0 ? <span> [+ {deltadeaths}] </span> : null}
+                </span>
+                <h3 className="font-weight-bold text-danger text-center ">
+                  <CountUp
+                    start={0}
+                    end={deaths}
+                    separator=","
+                    duration={2.5}
+                  />
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="text-right mt-3 font-weight-bold text-secondary">
+        Last Updated : {lastupdatedtime}
+      </p>
+    </div>
+  );
+};
+
+export default OriginalCard;
